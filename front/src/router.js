c4805adeccaacc9c9cccfdd20807e19ef373b94b
xsxiinnn <147054360+xsxiinnn@users.noreply.github.com>
2024-12-12 22:31:57 +0800

    Update router.js

--- front/src/router.js
+++ front/src/router.js
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import BreakfastMenu from './components/BreakfastMenu.vue';
-import ShoppingCart from './components/ShoppingCart.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import BreakfastMenu from '@/components/BreakfastMenu.vue'
+import ShoppingCart from '@/views/ShoppingCart.vue'
+import Checkout from '@/views/Checkout.vue'
+import OrderCompleted from '@/views/OrderCompleted.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'BreakfastMenu',
+    name: 'Menu',
     component: BreakfastMenu
   },
   {
     path: '/cart',
     name: 'ShoppingCart',
     component: ShoppingCart
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
+  },
+  {
+    path: '/order-completed',
+    name: 'OrderCompleted',
+    component: OrderCompleted
   }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-});
+})
 
-export default router;
+export default router
