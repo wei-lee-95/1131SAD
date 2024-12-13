33d954126689a428dc15fbaebddea11de6d09091
xsxiinnn <147054360+xsxiinnn@users.noreply.github.com>
2024-12-14 00:21:29 +0800

    Update OrderCompleted.vue

--- front/src/components/OrderCompleted.vue
+++ front/src/components/OrderCompleted.vue
@@ -6,7 +6,9 @@
 
     <p>感謝您的訂購！</p>
     <p>我們正在準備您的餐點。</p>
-    <router-link to="/">返回菜單</router-link>
+    <div class="button-container">
+      <router-link to="/" class="next-button">返回菜單</router-link>
+    </div>
   </div>
 </template>
 
@@ -15,3 +17,28 @@ export default {
   name: 'OrderCompleted'
 }
 </script>
+
+<style>
+.button-container {
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+}
+.next-button {
+  margin: 10px;
+  padding: 10px 20px;
+  background-color: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  font-size: 1em;
+  margin-top: 40px;
+  font-family: 'Noto Sans TC', sans-serif;
+}
+.next-button:hover {
+  background-color: #D2D2D2;
+  color: black;
+}
+</style>
